471be25db8dd4b114360ef9f9b1bd8af9c8a48f6
Gastón <gastongrimberg@gmail.com>
2021-04-23 23:22:37 -0300

    Delete token from checkout.php

--- checkout.php
+++ checkout.php
@@ -1,8 +1,6 @@
 <?php
     require_once 'vendor/autoload.php'; // You have to require the library from your Composer vendor folder
 
-    // test_access_token = "TEST-3570270571866309-042401-397feedc8a90254349eb4e1b28e48c05-99290503";
-
     MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398"); // Either Production or SandBox AccessToken
 
     $payment = new MercadoPago\Payment();
