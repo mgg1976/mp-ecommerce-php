05bba2ebd222758068cd1e5d1d942dc590698640
Gastón <gastongrimberg@gmail.com>
2021-04-28 23:52:23 -0300

    notifications.php fix

--- notifications.php
+++ notifications.php
@@ -2,6 +2,12 @@
     require_once 'vendor/autoload.php'; // You have to require the library from your Composer vendor folder
 
     MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
+    // Takes raw data from the request
+$json = file_get_contents('php://input');
+
+// Converts it into a PHP object
+$data = json_decode($json);
+var_dump($data);
     var_dump($_GET);
     switch($_POST["type"]) {
         case "payment":
