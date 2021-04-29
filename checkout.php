9249670d92fe097d1c2de35b2daeba6585fd3d7f
Gastón <gastongrimberg@gmail.com>
2021-04-29 00:11:27 -0300

    test webhook

--- checkout.php
+++ checkout.php
@@ -54,7 +54,7 @@
     );
     $preference->auto_return = "approved";
     $preference->external_reference = 'gastongrimberg@gmail.com';
-    $preference->notification_url = "https://mgg1976-mp-ecommerce-php.herokuapp.com/notifications.php";
+    $preference->notification_url = "https://webhook.site/e5c97c8b-5d02-41d7-84e9-66f84b5e6f9";
     $preference->save();
   ?>
 
--- notifications.php
+++ notifications.php
@@ -2,15 +2,8 @@
     require_once 'vendor/autoload.php'; // You have to require the library from your Composer vendor folder
 
     MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
-    // Takes raw data from the request
-$json = file_get_contents('php://input');
+    var_dump($_POST);
 
-// Converts it into a PHP object
-$data = json_decode($json);
-var_dump($data);
-    var_dump($_REQUEST);
-    $info = json_decode($this->input->raw_input_stream);
-    var_dump($info);
     // switch($_POST["type"]) {
     //     case "payment":
     //         $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
